232934f5213d300afb58879766b4e22240238fd3
ifcc <lvpeikang@gmail.com>
2024-09-13 09:22:47 +0800

    修改定时器

--- src/main/java/com/example/fakessh/controller/RecordController.java
+++ src/main/java/com/example/fakessh/controller/RecordController.java
@@ -7,11 +7,13 @@ import com.example.fakessh.enity.FakeSSHRecord;
 import com.example.fakessh.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
@@ -21,18 +23,38 @@ import java.util.Map;
 @RestController
 
 public class RecordController {
-    private final RecordService recordService;
 
+    private final RecordService recordService;
+    private Map <String, List<Map<String, Object>>> topData;
+    private Map<String, Integer> numData;
     @Autowired
     public RecordController(RecordService recordService) {
         this.recordService = recordService;
     }
-
     //    @GetMapping("/records")
 //    public IPage<FakeSSHRecord> getRecords(@RequestParam(value = "page", defaultValue = "1") int currentPage) {
 //        // 调用服务层获取分页数据
 //        return recordService.getRecordsWithPagination(currentPage);
 //    }
+    @PostConstruct
+    public void init(){
+        updateNumData();
+        updateTopData();
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void updateTopData(){
+        topData  = recordService.getTopten();
+        System.out.println("TopUpdated");
+    }
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void updateNumData(){
+        numData  = recordService.getFourIntegers();
+        System.out.println("NumUpdated");
+    }
+
+
+
     @GetMapping("/records")
     public IPage<FakeSSHRecord> getRecords(@RequestParam(value = "page", defaultValue = "1") int currentPage,
                                            @RequestParam(value = "size", defaultValue = "100") int size,
@@ -45,10 +67,12 @@ public class RecordController {
 
     @GetMapping("/getfournum")
     public Map<String ,Integer> getFourNum(){
-        return recordService.getFourIntegers();
+        return this.numData;
     }
     @GetMapping("/gettoptenaddrs")
     public Map<String ,List<Map<String,Object>>> getTopTenAddrs(){
-       return recordService.getTopten();
+        return this.topData;
     }
 }
+
+
